ca2e30a26255725b29572aef4684ec6ea7e060ba
Kasis11 <126655386+Kasis11@users.noreply.github.com>
2024-12-22 12:08:53 +0530

    Add files via upload

---
+++ Django_Task_3_KasisRana/Code/JwtCustomAuth/JwtAuth/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import SignupView, LoginView,ProtectedView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('protected/', ProtectedView.as_view(), name='protected'),
+]
